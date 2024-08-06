6f7788abe6d93120916f1cd61b75165b3d52add9
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-08-06 13:46:54 -0300

    Criando Front end

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Formulario from './Tabela';
+import Tabela from './Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario/>
+      <Tabela />
+    </div>
+  );
+}
+
+export default App;
