0e81919ba8c60315ccd65138f00543886fe82853
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-12-18 11:40:24 -0300

    Updata UserPage

--- front/src/Components/UserPage.jsx
+++ front/src/Components/UserPage.jsx
@@ -62,7 +62,6 @@ function UserPage() {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
 
-   
     fetchUserData(userId, token, setUserData);
     fetchUserGames(userId, token, setGames);
     fetchUserFavorites(userId, token, setFavorites);
@@ -141,142 +140,135 @@ function UserPage() {
       <div className='content'>
         <div className="user-page">
           <div className="profile-header">
-            {/* Verificando se os dados do usuário estão carregados */}
-            {userData.username ? (
-              <>
-                <div className="profile-image-container">
-                  <img
-                    src={userData.profileImage || defaultProfileImage}
-                    alt="User"
-                    className="profile-image"
-                  />
-                  <input
-                    type="file"
-                    id="imageInput"
-                    style={{ display: 'none' }}
-                    accept="image/*"
-                    onChange={handleImageChange}
-                  />
-                  <button
-                    className="change-image-button"
-                    onClick={() => document.getElementById('imageInput').click()}
-                  >
-                    <i className="fas fa-camera"></i> Alterar Imagem
-                  </button>
-                </div>
-                <h1>
+            <div className="profile-image-container">
+              <img
+                src={userData.profileImage || defaultProfileImage}
+                alt="User"
+                className="profile-image"
+              />
+              <input
+                type="file"
+                id="imageInput"
+                style={{ display: 'none' }}
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+              <button
+                className="change-image-button"
+                onClick={() => document.getElementById('imageInput').click()}
+              >
+                <i className="fas fa-camera"></i> Alterar Imagem
+              </button>
+            </div>
+            <h1>
+              {editMode ? (
+                <textarea
+                  name="username"
+                  value={userData.username}
+                  onChange={handleChange}
+                />
+              ) : (
+                <p>{userData.username}</p>
+              )}
+            </h1>
+            <div className="profile-details">
+              <div className="profile-info">
+                <label>
+                  Email:
+                  <p>{userData.email}</p> {/* Email fixo, não editável */}
+                </label>
+                <label>
+                  Nome Completo:
                   {editMode ? (
                     <textarea
-                      name="username"
-                      value={userData.username}
+                      name="fullname"
+                      value={userData.fullname}
                       onChange={handleChange}
                     />
                   ) : (
-                    <p>{userData.username}</p>
+                    <p>{userData.fullname}</p>
                   )}
-                </h1>
-                <div className="profile-details">
-                  <div className="profile-info">
-                    <label>
-                      Email:
-                      <p>{userData.email}</p> {/* Email fixo, não editável */}
-                    </label>
-                    <label>
-                      Nome Completo:
-                      {editMode ? (
-                        <textarea
-                          name="fullname"
-                          value={userData.fullname}
-                          onChange={handleChange}
-                        />
-                      ) : (
-                        <p>{userData.fullname}</p>
-                      )}
-                    </label>
-                    <label>
-                      Biografia:
-                      {editMode ? (
-                        <textarea
-                          name="bio"
-                          value={userData.bio}
-                          onChange={handleChange}
-                        />
-                      ) : (
-                        <p>{userData.bio}</p>
-                      )}
-                    </label>
-                  </div>
-                  <button onClick={editMode ? handleSave : handleEdit} className="edit-button">
-                    <i className={`fas ${editMode ? 'fa-save' : 'fa-edit'}`}></i> {editMode ? "Salvar" : "Editar"}
-                  </button>
-                  <button onClick={handleLogout} className="logout-button">
-                    <i className="fas fa-sign-out-alt"></i> Sair
-                  </button>
-                </div>
-  
-                {/* Header para alternar entre jogos adquiridos e favoritos */}
-                <div className="tabs">
-                  <button
-                    className={`tab-button ${activeTab === 'games' ? 'active' : ''}`}
-                    onClick={() => setActiveTab('games')}
-                  >
-                    <i className="fas fa-gamepad"></i> Jogos Adquiridos
-                  </button>
-                  <button
-                    className={`tab-button ${activeTab === 'favorites' ? 'active' : ''}`}
-                    onClick={() => setActiveTab('favorites')}
-                  >
-                    <i className="fas fa-heart"></i> Jogos Favoritos
-                  </button>
-                </div>
-  
-                {/* Conteúdo das seções baseadas na aba ativa */}
-                <div className="games-wrapper">
-                  {activeTab === 'games' && (
-                    <div className="games-section">
-                      <h2>Jogos Adquiridos</h2>
-                      <ul className='games-list'>
-                        {games.length === 0 ? (
-                          <p>Você ainda não adquiriu jogos.</p>
-                        ) : (
-                          games.map((game) => (
-                            <li key={game.id}>
-                              <img src={game.imageUrl || `data:image/jpeg;base64,${game.image}`} alt={game.name} className="cart-item-image" />
-                              <p>{game.name}</p>
-                            </li>
-                          ))
-                        )}
-                      </ul>
-                    </div>
+                </label>
+                <label>
+                  Biografia:
+                  {editMode ? (
+                    <textarea
+                      name="bio"
+                      value={userData.bio}
+                      onChange={handleChange}
+                    />
+                  ) : (
+                    <p>{userData.bio}</p>
                   )}
-                  {activeTab === 'favorites' && (
-                    <div className="favorites-section">
-                      <h2>Jogos Favoritos</h2>
-                      <ul className='favorites-list-Page'>
-                        {favorites.length === 0 ? (
-                          <p>Você ainda não possui jogos favoritos.</p>
-                        ) : (
-                          favorites.map((game) => (
-                            <li key={game.id} onClick={() => handleRedirect(game.id)}>
-                              <img src={game.imageUrl || `data:image/jpeg;base64,${game.image}`} alt={game.name} className="cart-item-image" />
-                              <p>{game.name}</p>
-                            </li>
-                          ))
-                        )}
-                      </ul>
-                    </div>
-                  )}
+                </label>
+              </div>
+              <button onClick={editMode ? handleSave : handleEdit} className="edit-button">
+                <i className={`fas ${editMode ? 'fa-save' : 'fa-edit'}`}></i> {editMode ? "Salvar" : "Editar"}
+              </button>
+              <button onClick={handleLogout} className="logout-button">
+                <i className="fas fa-sign-out-alt"></i> Sair
+              </button>
+            </div>
+
+            {/* Header para alternar entre jogos adquiridos e favoritos */}
+            <div className="tabs">
+              <button
+                className={`tab-button ${activeTab === 'games' ? 'active' : ''}`}
+                onClick={() => setActiveTab('games')}
+              >
+                <i className="fas fa-gamepad"></i> Jogos Adquiridos
+              </button>
+              <button
+                className={`tab-button ${activeTab === 'favorites' ? 'active' : ''}`}
+                onClick={() => setActiveTab('favorites')}
+              >
+                <i className="fas fa-heart"></i> Jogos Favoritos
+              </button>
+            </div>
+
+            {/* Conteúdo das seções baseadas na aba ativa */}
+            <div className="games-wrapper">
+              {activeTab === 'games' && (
+                <div className="games-section">
+                  <h2>Jogos Adquiridos</h2>
+                  <ul className='games-list'>
+                    {games.length === 0 ? (
+                      <p>Você ainda não adquiriu jogos.</p>
+                    ) : (
+                      games.map((game) => (
+                        <li key={game.id}>
+                          <img src={game.imageUrl || `data:image/jpeg;base64,${game.image}`} alt={game.name} className="cart-item-image" />
+                          <p>{game.name}</p>
+                        </li>
+                      ))
+                    )}
+                  </ul>
                 </div>
-              </>
-            ) : (
-              <p>Carregando...</p>
-            )}
+              )}
+              {activeTab === 'favorites' && (
+                <div className="favorites-section">
+                  <h2>Jogos Favoritos</h2>
+                  <ul className='favorites-list-Page'>
+                    {favorites.length === 0 ? (
+                      <p>Você ainda não possui jogos favoritos.</p>
+                    ) : (
+                      favorites.map((game) => (
+                        <li key={game.id} onClick={() => handleRedirect(game.id)}>
+                          <img src={game.imageUrl || `data:image/jpeg;base64,${game.image}`} alt={game.name} className="cart-item-image" />
+                          <p>{game.name}</p>
+                        </li>
+                      ))
+                    )}
+                  </ul>
+                </div>
+              )}
+            </div>
+
           </div>
         </div>
       </div>
     </>
   );
-  
 }
 
 export default UserPage;
